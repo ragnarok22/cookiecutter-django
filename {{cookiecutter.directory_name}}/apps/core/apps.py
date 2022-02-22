257e81366ecf018eeddc9cec61d1dcdea49bfaf5
Reinier Hernández <sasuke.reinier@gmail.com>
2022-02-22 11:40:45 -0500

    chore: create core app

---
+++ {{cookiecutter.directory_name}}/apps/core/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.core'
+    label = 'core'
+    verbose_name = 'Core'
