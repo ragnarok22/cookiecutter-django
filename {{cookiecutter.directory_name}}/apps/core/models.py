d81c0ec1a94480a2e2ceb81fd003207e3bf902b7
Reinier Hernández <sasuke.reinier@gmail.com>
2022-02-22 11:42:02 -0500

    chore: add singleton and timestamped model on core

--- {{cookiecutter.directory_name}}/apps/core/models.py
+++ {{cookiecutter.directory_name}}/apps/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """
+    A model to reuse the `created_at` and `updated_at` fields
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SingletonModel(models.Model):
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def load(cls):
+        obj, created = cls.objects.get_or_create(pk=1)
+        return obj
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
