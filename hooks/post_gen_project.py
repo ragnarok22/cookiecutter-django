408d8726939932f44e8c3951db32f0d61d386dba
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-05 12:22:21 -0400

    feat: add package option choice

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+import os
+
+REMOVE_PATHS = [
+    '{% if cookiecutter.packaging != "pip" %} requirements.txt {% endif %}',
+    '{% if cookiecutter.packaging != "poetry" %} poetry.lock {% endif %}',
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            os.rmdir(path)
+        else:
+            os.unlink(path)
